2ee898886a3ea55292b7871b671f2843388b32dc
mohammadrezaahmadi2003 <115949581+mohammadrezaahmadi2003@users.noreply.github.com>
2022-10-26 00:11:10 +0330

    Create Ex1_2.py

---
+++ Ex1/Ex1_2.py
@@ -0,0 +1,9 @@
+#base = b
+#hight = h
+def triangle(b, h):
+    area = (b*h)/2
+    print("the triangle is", area)
+#example:
+triangle(3, 6)
+
+#help: you give number as base and hight and then area is calculated according to the formula(base*hight/2)
