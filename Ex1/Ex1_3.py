de8499e787c171a32711bd005c5dd7a47a6bb546
mohammadrezaahmadi2003 <115949581+mohammadrezaahmadi2003@users.noreply.github.com>
2022-10-26 00:13:24 +0330

    Create Ex1_3.py

---
+++ Ex1/Ex1_3.py
@@ -0,0 +1,10 @@
+
+def sides_of_triangle(side1, side2):
+    formula = (side1 + side2)-1
+    print("Maximum Edge of a Triangle is", formula)
+
+
+#example:
+sides_of_triangle(6, 5)
+#int -> int
+#help:to get Maximum Edge of a Triangle you need to have the value of two sides of triangle and then add value of both of them then subtract one unit
