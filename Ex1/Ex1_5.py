e63948509ddd4351be702d1c9c382f6dafa1c89a
mohammadrezaahmadi2003 <115949581+mohammadrezaahmadi2003@users.noreply.github.com>
2022-10-26 00:14:39 +0330

    Create Ex1_5.py

---
+++ Ex1/Ex1_5.py
@@ -0,0 +1,11 @@
+age = float(input("please enter your age"))
+if age <= 1.0:
+    print("infant")
+if 1.0 < age < 13.0:
+    print("child")
+if 13.0 < age < 20.0:
+    print("teenager")
+if 20.0 < age:
+    print("adult")
+#float -> str
+#help:you enter values and then according to your number ,the category of that will display
